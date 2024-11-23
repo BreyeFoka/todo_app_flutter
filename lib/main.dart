d0096836d788fff176afc1ef692ec46786616909
Breye Foka <breyefokalagloire@gmail.com>
2024-11-23 19:16:13 +0100

    innitial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/pages/to_do.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ToDoPage(),
+    );
+  }
+}
